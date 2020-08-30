6286a7ffbafdbacb47919a40a45bc0b5d1d0ce32
Preston Davis <prestonseandavis101@yahoo.com>
2020-08-30 15:46:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize"}:"
     holiday.each do |holiday, item|
-    puts "#{holiday.to.split("_").map}"
+    puts "#{holiday.to.split("_").map{|i| i.capitalize}.join(" ")
 
 end
 
