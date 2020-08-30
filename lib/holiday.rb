711b8e08ea9efd7efcbc419a5656a3d6d4243461
Preston Davis <prestonseandavis101@yahoo.com>
2020-08-30 15:36:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -35,8 +35,7 @@ holiday_hash[:spring][:memorial_day].push(supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
+holiday_hash[season][holiday_name] = supply_array
 
 end
 
