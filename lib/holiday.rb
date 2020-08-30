d1d05f9fbf3dd5549f8663fb92f7b2021f2c1a26
Preston Davis <prestonseandavis101@yahoo.com>
2020-08-30 15:30:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,7 +22,8 @@ end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash
-  [:winter].each do |holiday, decorations| decorations << supply
+  [:winter].each do |holiday, decorations| 
+    decorations << supply
 end
 
 end
